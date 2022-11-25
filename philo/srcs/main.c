f6a42fccb677a6f6bfedce748762e5e5d54a2156
Francisco Pires <98482270+PireXa@users.noreply.github.com>
2022-11-25 12:45:50 +0000

    Add files via upload

---
+++ philo/srcs/main.c
@@ -0,0 +1,88 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   PHILOSOPHERS                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rdas-nev <rdas-nev@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 4242/42/42 42:42:42 by rdas-nev          #+#    #+#             */
+/*   Updated: 4242/42/42 42:42:42 by rdas-nev         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../inc/philo.h"
+
+// int array
+// 0: philo number
+// 1: sleeping or not
+// //////////////////
+// long long int array
+// 0: time of last eat
+// 1: time of last sleep
+// //////////////////
+// static int array
+// 0: thread init check
+// 1: death checker
+// 2: eat counter
+
+t_tt	*build_threadthings(t_tt *p, int n)
+{
+	p->i = malloc(sizeof(int) * 2);
+	p->i[0] = n;
+	p->i[1] = 0;
+	p->s = (struct timeval *)malloc(sizeof(struct timeval));
+	gettimeofday(p->s, NULL);
+	p->li = malloc(sizeof(long long int) * 2);
+	p->li[0] = gt(*p->s);
+	p->li[1] = 0;
+	/*if (p->i[0] % 2 == 0)
+		usleep(5000);*/
+	return (p);
+}
+
+void	*f_thread(void *m)
+{
+	static int		*si;
+	t_tt			*p;
+	t_philo			*a;
+
+	si = (int [3]){0, 0, 0};
+	init_thread(m);
+	a = (t_philo *)((t_m *) m)->b[si[0]];
+	p = (t_tt *)((t_m *) m)->t[si[0]++];
+	while (si[0] != *(((t_m *) m)->ph_n))
+		;
+	p = build_threadthings(p, *a->phn);
+	while (*((t_m *)m)->morreu == 0 && (*a->ecnt == -1 || si[2] < *a->ecnt * si[0]))
+	{
+		if (!me_dead(*((t_m *)m)->die_t, p->li[0], *p->s))
+		{
+			si[2] += megacoiso(((t_m *)m), p, a);
+			if (sleeper_2((t_m *)m, p, a) == 1)
+				return (NULL);
+		}
+		else
+		{
+			printf("morreu\n");
+			*((t_m *)m)->morreu = philodied(a, gt(*p->s), p->i[0], 0);
+		}
+	}
+	endthread(a, gt(*p->s), p->i[0], p);
+	return (NULL);
+}
+
+int	main(int ac, char **av)
+{
+	t_m	*m;
+
+	if (inputcheck(ac, av) == 1)
+		return (1);
+	else
+	{
+		m = malloc(sizeof(t_m));
+		masterbuilder(m, av);
+		threaddoer(m, av);
+		masterdestroyer(m);
+		return (0);
+	}
+}
---
+++ philo/srcs/threadutils.c
@@ -0,0 +1,83 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   PHILOSOPHERS                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rdas-nev <rdas-nev@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 4242/42/42 42:42:42 by rdas-nev          #+#    #+#             */
+/*   Updated: 4242/42/42 42:42:42 by rdas-nev         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../inc/philo.h"
+
+void	endthread(t_philo *a, long long int t, int i, t_tt *ta)
+{
+	if (*a->status == 1)
+		printf("%s%lld ms %s%d died\n%s", P, t, R, i, RE);
+	free(a->status);
+	free(a->phn);
+	free(a->ecnt);
+	free(a);
+	free(ta->i);
+	free(ta->s);
+	free(ta->li);
+	free(ta);
+}
+
+int	philodied(t_philo *a, long long int t, int i, int flag)
+{
+	if (flag == 0)
+		*a->status = 1;
+	else if (flag == 1)
+	{
+		*a->status = 1;
+		printf("%s%lld ms %s%d died\n%s", P, t, R, i, RE);
+		free(a->status);
+		free(a->phn);
+		free(a->ecnt);
+		free(a);
+	}
+	return (1);
+}
+
+void	forkaction(t_philo *a, long long int t, int i)
+{
+	if (i < 0)
+	{
+		*a->e->q = 2;
+		printf("%s%lld ms %s%d has taken a fork\n", P, t, Y, -i);
+		*a->d->q = 2;
+		printf("%s%lld ms %s%d has taken a fork\n", P, t, Y,-i);
+		return ;
+	}
+	if (i > 0 && i < 1000)
+	{
+		*a->e->q = 2;
+		printf("%s%lld ms %s%d has taken a fork\n", P, t, Y, i);
+		return ;
+	}
+	*a->e->q = 1;
+	*a->d->q = 1;
+}
+
+int	eat(t_philo *a, t_m *m, t_tt *p, int *i)
+{
+	forkaction(a, gt(*p->s), -p->i[0]);
+	pthread_mutex_unlock(((t_m *) m)->mt1);
+	printf(P"%lld ms "G"%d is eating\n", gt(*p->s), p->i[0]);
+	usleep(*((t_m *)m)->eat_t * 1000);
+	pthread_mutex_lock(((t_m *) m)->mt2);
+	forkaction(a, gt(*p->s), p->i[0] * 1000);
+	pthread_mutex_unlock(((t_m *) m)->mt2);
+	i[1] = 1;
+	return (1);
+}
+
+void	init_thread(t_m *m)
+{
+	pthread_mutex_lock(((t_m *) m)->mt3);
+	usleep(500 * 1000);
+	pthread_mutex_unlock(((t_m *) m)->mt3);
+}
